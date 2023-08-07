575631d914ff0f796d2e7867f29be0fb3243ca12
hiranya22 <141381165+hiranya22@users.noreply.github.com>
2023-08-07 10:35:20 +0530

    Add files via upload

---
+++ Supermarket-Hiranya/src/pos/mvc/PosMvc.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pos.mvc;
+
+import pos.mvc.view.CustomerView;
+import pos.mvc.view.ItemView;
+import pos.mvc.view.OrderView;
+
+/**
+ *
+ * @author Hiranya
+ */
+public class PosMvc {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       new OrderView().setVisible(true);
+    }
+    
+}
